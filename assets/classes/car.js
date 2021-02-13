e2f3635848108528e422f03ef164a3cb528deed7
Hristo Dzhambazki <hristo.dzhambazki@gmail.com>
2021-02-13 14:44:12 +0200

    added common info class

---
+++ assets/classes/car.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor(brand, model, year, engine, power, euroSt, gear, category, mileage, color, region, others, desctiption) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.engine = engine;
+        this.power = power;
+        this.euroSt = euroSt;
+        this.gear = gear;
+        this.category = category;
+        this.mileage = mileage;
+        this.color = color;
+        this.region = region;
+        this.others = others;
+        this.desctiption = desctiption;
+    }
+}
---
+++ assets/classes/commonInfo.js
@@ -0,0 +1,12 @@
+class CommonInfo {
+    constructor(state, color, price, currency, region, description, mobile, email) {
+        this.state = state;
+        this.color = color;
+        this.price = price;
+        this.currency = currency;
+        this.region = region;
+        this.description = description;
+        this.mobile = mobile;
+        this.email = email;
+    }
+}
