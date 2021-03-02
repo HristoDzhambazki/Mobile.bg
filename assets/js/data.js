39ac440da20b61b445e2c2e6e5a933d36ae0d1a3
Hristo Dzhambazki <hristo.dzhambazki@gmail.com>
2021-03-02 18:19:16 +0200

    created car service

---
+++ assets/js/data.js
@@ -0,0 +1,205 @@
+let arrayData = [
+    {
+        state: 'stara',
+        price: 5000,
+        currency: 'leva',
+        region: 'Sofia',
+
+        images: [
+            'car1-1.jpg',
+            'car1-2.jpg',
+            'car1-3.jpg',
+        ],
+
+
+        brand: 'Toyota',
+        model: 'Yaris',
+        year: 2009,
+        engine: 'benzinov',
+        power: 69,
+        euroSt: 'Euro 4',
+        gear: 'Rychna',
+        category: 'Hechbek',
+        mileage: 144000,
+        color: 'black',
+        others: {
+            comfort: ['bordkompiuter', 'el.ogledala', 'el.stykla'],
+        },
+
+        description: ' Нов внос ! Автомобил проверен в наш собствен сервиз и се продава с 6 МЕСЕЦА ГАРАНЦИЯ',
+
+        seller: 'Variant M',
+        mobile: 0897937799,
+        email: 'variantM@abv.bg'
+    },
+    {
+        state: 'stara',
+        price: 60000,
+        currency: 'leva',
+        region: 'Ruse',
+
+        images: [
+            'car2-1.jpg',
+            'car2-2.jpg',
+            'car2-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'dizelov',
+        power: 258,
+        euroSt: 'Euro 4',
+        gear: 'avtomatichna',
+        category: 'sedan',
+        mileage: 199000,
+        color: 'hameleon',
+        others: {
+            other: ['barter'],
+        },
+
+        description: '=== S63 AMG FACELIFT === === LONG БИЗНЕС КЛАС === === Внос ГЕРМАНИЯ ===',
+
+        seller: '',
+        mobile: 0886553555,
+        email: ''
+    },
+    {
+        state: 'stara',
+        price: 60000,
+        currency: 'leva',
+        region: 'Ruse',
+
+        images: [
+            'car3-1.jpg',
+            'car3-2.jpg',
+            'car3-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'dizelov',
+        power: 258,
+        euroSt: 'Euro 4',
+        gear: 'avtomatichna',
+        category: 'sedan',
+        mileage: 199000,
+        color: 'hameleon',
+        others: {
+            other: ['barter'],
+        },
+
+        description: '=== S63 AMG FACELIFT === === LONG БИЗНЕС КЛАС === === Внос ГЕРМАНИЯ ===',
+
+        seller: '',
+        mobile: 0886553555,
+        email: ''
+    },
+    {
+        state: 'stara',
+        price: 9990,
+        currency: 'leva',
+        region: 'Dupnica',
+
+        images: [
+            'car4-1.jpg',
+            'car4-2.jpg',
+            'car4-3.jpg',
+        ],
+
+
+        brand: 'Citroen',
+        model: 'C5',
+        year: 2010,
+        engine: 'dizelov',
+        power: 163,
+        euroSt: 'Euro 5',
+        gear: 'avtomatichna',
+        category: 'sedan',
+        mileage: 168000,
+        color: 'tymno siv',
+        others: {
+            comfort: ['Bluetooth', 'DVD', 'USB'],
+            other: ['lizing', 'nov vnos', 'servizna knijka']
+        },
+
+        description: 'Всичко по автомобила работи. Отлична визия. Безупречен технически.',
+
+        seller: 'RUNA LTD',
+        mobile: 0898705582,
+        email: 'runa@abv.bg'
+    },
+    {
+        state: 'stara',
+        price: 28000,
+        currency: 'leva',
+        region: 'Plovdiv',
+
+        images: [
+            'car5-1.jpg',
+            'car5-2.jpg',
+            'car5-3.jpg',
+        ],
+
+
+        brand: 'VW',
+        model: 'Jetta',
+        year: 2019,
+        engine: 'benzinov',
+        power: 150,
+        euroSt: '',
+        gear: 'avtomatichna',
+        category: 'sedan',
+        mileage: 35000,
+        color: 'sin',
+        others: {
+            comfort: ['Auto Start Stop function', 'Bluetooth ', 'USB'],
+            other: ['Бартер', '4x4', 'Лизинг']
+        },
+
+        description: 'Черен интериорен салон',
+
+        seller: 'AutoHaus.BG',
+        mobile: 0884777045,
+        email: 'auto@abv.bg'
+    },
+    {
+        state: 'stara',
+        price: 7000,
+        currency: 'leva',
+        region: 'Burgas',
+
+        images: [
+            'car6-1.jpg',
+            'car6-2.jpg',
+            'car6-3.jpg',
+        ],
+
+
+        brand: 'BMW',
+        model: '120',
+        year: 2006,
+        engine: 'dizelov',
+        power: 163,
+        euroSt: 'Euro 5',
+        gear: 'rychna',
+        category: 'hechbek',
+        mileage: 176000,
+        color: 'tymno sin',
+        others: {
+            comfort: ['Безключово палене', 'Бордкомпютър ', 'Ел. Огледала'],
+            other: ['Бартер', 'С регистрация', 'Сервизна книжка']
+        },
+
+        description: 'Колата е в перфектно састояние обслужена много гледана и подържена приемам бартери',
+
+        seller: '',
+        mobile: 0876006672,
+        email: ''
+    }
+]
+
+console.log(arrayData.length);
