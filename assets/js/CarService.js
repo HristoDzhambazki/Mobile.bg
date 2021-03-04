a88397cb12bd94fbfebab725d8810ab39f4ba8b5
GramatikovM <GramatikovM@yahoo.com>
2021-03-04 13:37:06 +0200

    updated main page and added publish page

--- assets/js/CarService.js
+++ assets/js/CarService.js
@@ -50,6 +50,43 @@ let carStorage = (function () {
             this.list.splice(index, 1);
         }
     }
-
+    
     return new CarStorage();
 })();
+
+let id = 0;
+
+    class Car {
+        constructor(state, price, currency, images, brand, model, year, engine, power, euroSt, gear, category, mileage, color, region, others, desctiption, seller, mobile, email) {
+
+            this.id = ++id;
+
+            this.state = state;
+            this.price = price;
+            this.currency = currency;
+            this.region = region;
+
+            this.images = images;
+
+            this.information = {
+                desctiption,
+                seller,
+                mobile,
+                email
+            }
+
+            this.components = {
+                brand,
+                model,
+                year,
+                engine,
+                power,
+                euroSt,
+                gear,
+                category,
+                mileage,
+                color,
+                others,
+            }
+        }
+    }
