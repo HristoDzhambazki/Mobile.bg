3b999564e5db6fe6fc43fb59c652dc28d3130e0c
Hristo Dzhambazki <hristo.dzhambazki@gmail.com>
2021-03-14 15:50:14 +0200

    added functionality to show ads in search results

--- assets/data/FakeCarsData.js
+++ assets/data/FakeCarsData.js
@@ -156,6 +156,321 @@ let arrayData = [
             comfort: ['Безключово палене', 'Бордкомпютър', 'Ел. огледала'],
             others: ['Бартер', 'С регистрация', 'Сервизна книжка']
         },
+    }, {
+        price: 5000,
+        currency: 'лв.',
+        region: 'София',
+
+        images: [
+            'car1-1.jpg',
+            'car1-2.jpg',
+            'car1-3.jpg',
+        ],
+
+
+        brand: 'Toyota',
+        model: 'Yaris',
+        year: 2009,
+        engine: 'Бензинов',
+        power: 69,
+        euroSt: 'Евро 4',
+        gear: 'Ръчна',
+        category: 'Хечбек',
+        mileage: 144000,
+        color: 'Черен',
+        extras: {
+            comfort: ['Бордкомпютър', 'Ел. огледала', 'Ел. стъкла'],
+        },
+    },
+    {
+        price: 60000,
+        currency: 'лв.',
+        region: 'Русе',
+
+        images: [
+            'car2-1.jpg',
+            'car2-2.jpg',
+            'car2-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'Дизелов',
+        power: 258,
+        euroSt: 'Евро 4',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 199000,
+        color: 'Хамелеон',
+        extras: {
+            others: ['Бартер'],
+        },
+    },
+    {
+        price: 60000,
+        currency: 'лв.',
+        region: 'Русе',
+
+        images: [
+            'car2-1.jpg',
+            'car2-2.jpg',
+            'car2-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'Дизелов',
+        power: 258,
+        euroSt: 'Евро 4',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 199000,
+        color: 'Хамелеон',
+        extras: {
+            others: ['Бартер'],
+        },
+    },
+    {
+        price: 9990,
+        currency: 'лв.',
+        region: 'Дупница',
+
+        images: [
+            'car4-1.jpg',
+            'car4-2.jpg',
+            'car4-3.jpg',
+        ],
+
+
+        brand: 'Citroen',
+        model: 'C5',
+        year: 2010,
+        engine: 'Дизелов',
+        power: 163,
+        euroSt: 'Евро 5',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 168000,
+        color: 'Тъмно сив',
+        extras: {
+            comfort: ['Bluetooth', 'DVD, TV', 'USB'],
+            others: ['Лизинг', 'Нов внос', 'Сервизна книжка']
+        },
+    },
+    {
+        price: 28000,
+        currency: 'USD',
+        region: 'Пловдив',
+
+        images: [
+            'car5-1.jpg',
+            'car5-2.jpg',
+            'car5-3.jpg',
+        ],
+
+        brand: 'VW',
+        model: 'Jetta',
+        year: 2019,
+        engine: 'Бензинов',
+        power: 150,
+        euroSt: '',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 35000,
+        color: 'Син',
+        extras: {
+            comfort: ['Auto Start Stop function', 'Bluetooth', 'USB'],
+            others: ['Бартер', '4x4', 'Лизинг']
+        },
+    },
+    {
+        price: 7000,
+        currency: 'лв.',
+        region: 'Бургас',
+
+        images: [
+            'car6-1.jpg',
+            'car6-2.jpg',
+            'car6-3.jpg',
+        ],
+
+
+        brand: 'BMW',
+        model: '120',
+        year: 2006,
+        engine: 'Дизелов',
+        power: 163,
+        euroSt: 'Евро 5',
+        gear: 'Ръчна',
+        category: 'Хечбек',
+        mileage: 176000,
+        color: 'Тъмно син',
+        extras: {
+            comfort: ['Безключово палене', 'Бордкомпютър', 'Ел. огледала'],
+            others: ['Бартер', 'С регистрация', 'Сервизна книжка']
+        },
+    },
+    {
+        price: 5000,
+        currency: 'лв.',
+        region: 'София',
+
+        images: [
+            'car1-1.jpg',
+            'car1-2.jpg',
+            'car1-3.jpg',
+        ],
+
+
+        brand: 'Toyota',
+        model: 'Yaris',
+        year: 2009,
+        engine: 'Бензинов',
+        power: 69,
+        euroSt: 'Евро 4',
+        gear: 'Ръчна',
+        category: 'Хечбек',
+        mileage: 144000,
+        color: 'Черен',
+        extras: {
+            comfort: ['Бордкомпютър', 'Ел. огледала', 'Ел. стъкла'],
+        },
+    },
+    {
+        price: 60000,
+        currency: 'лв.',
+        region: 'Русе',
+
+        images: [
+            'car2-1.jpg',
+            'car2-2.jpg',
+            'car2-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'Дизелов',
+        power: 258,
+        euroSt: 'Евро 4',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 199000,
+        color: 'Хамелеон',
+        extras: {
+            others: ['Бартер'],
+        },
+    },
+    {
+        price: 60000,
+        currency: 'лв.',
+        region: 'Русе',
+
+        images: [
+            'car2-1.jpg',
+            'car2-2.jpg',
+            'car2-3.jpg',
+        ],
+
+
+        brand: 'Mercedes-Benz',
+        model: 'S 350',
+        year: 2014,
+        engine: 'Дизелов',
+        power: 258,
+        euroSt: 'Евро 4',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 199000,
+        color: 'Хамелеон',
+        extras: {
+            others: ['Бартер'],
+        },
+    },
+    {
+        price: 9990,
+        currency: 'лв.',
+        region: 'Дупница',
+
+        images: [
+            'car4-1.jpg',
+            'car4-2.jpg',
+            'car4-3.jpg',
+        ],
+
+
+        brand: 'Citroen',
+        model: 'C5',
+        year: 2010,
+        engine: 'Дизелов',
+        power: 163,
+        euroSt: 'Евро 5',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 168000,
+        color: 'Тъмно сив',
+        extras: {
+            comfort: ['Bluetooth', 'DVD, TV', 'USB'],
+            others: ['Лизинг', 'Нов внос', 'Сервизна книжка']
+        },
+    },
+    {
+        price: 28000,
+        currency: 'USD',
+        region: 'Пловдив',
+
+        images: [
+            'car5-1.jpg',
+            'car5-2.jpg',
+            'car5-3.jpg',
+        ],
+
+        brand: 'VW',
+        model: 'Jetta',
+        year: 2019,
+        engine: 'Бензинов',
+        power: 150,
+        euroSt: '',
+        gear: 'Автоматична',
+        category: 'Седан',
+        mileage: 35000,
+        color: 'Син',
+        extras: {
+            comfort: ['Auto Start Stop function', 'Bluetooth', 'USB'],
+            others: ['Бартер', '4x4', 'Лизинг']
+        },
+    },
+    {
+        price: 7000,
+        currency: 'лв.',
+        region: 'Бургас',
+
+        images: [
+            'car6-1.jpg',
+            'car6-2.jpg',
+            'car6-3.jpg',
+        ],
+
+
+        brand: 'BMW',
+        model: '120',
+        year: 2006,
+        engine: 'Дизелов',
+        power: 163,
+        euroSt: 'Евро 5',
+        gear: 'Ръчна',
+        category: 'Хечбек',
+        mileage: 176000,
+        color: 'Тъмно син',
+        extras: {
+            comfort: ['Безключово палене', 'Бордкомпютър', 'Ел. огледала'],
+            others: ['Бартер', 'С регистрация', 'Сервизна книжка']
+        },
     }
 ]
 
--- assets/js/SearchResultsController.js
+++ assets/js/SearchResultsController.js
@@ -1,5 +1,5 @@
 //Using search object from SearchController.js
-let resultObj = carStorage.filter(searchObj);
+let resultsObj = '';
 
 //DOM Selectors
 let newSearchElement = getById('newSearch');
@@ -10,25 +10,145 @@ let resultsCategory = getById('searchResultsCategory');
 let resultsOrderedBy = getById('searchResultsOrderedBy');
 let resultsFeatures = getById('searchResultsFeatures');
 let resultsExtras = getById('searchResultsExtras');
+let mainResultsElement = getById('mainResults')
+let paginationElement = getById('searchResultsPagination');
 
 //Buttons
 let searchBtn = getById('searchButton');
 
 //Events
 searchBtn.addEventListener('click', () => {
-    console.log(searchObj);
-
+    resultsObj = carStorage.filter(searchObj);
+    mainResultsElement.innerHTML = '';
     // location.hash = '#searchResultsPage';
-    // // resultObj = carStorage.filter(searchObj);
-    // // console.log(resultObj);
+    // // resultsObj = carStorage.filter(searchObj);
+    // // console.log(resultsObj);
 
-    // renderResultInfoHeading();
     renderResultsMainInfo();
+    renderPagination();
+
+    resultsObj.forEach(ad => {
+        renderAd(ad);
+    })
 })
 
 newSearchAnchor.addEventListener('click', resetSearchMenu);
 
 // Functions
+function renderPagination() {
+    let adsCount = resultsObj.length;
+    let pagesCount = Math.floor(adsCount / 5);
+
+    for (let i = 0; i < pagesCount; i++) {
+
+
+    }
+}
+
+function renderAd(ad) {
+    let card = createElement('div', 'searchResultCard');
+
+    let upperBody = createElement('div', 'cardUpperBody');
+
+    let cardImageDiv = createElement('div', 'cardImageDiv')
+    let anchorImg = createElement('a');
+    let cardMainImg = createElement('img', 'cardMainImg');
+    cardMainImg.src = 'assets/images/cars/' + ad.images.value[0];
+
+    anchorImg.append(cardMainImg);
+    cardImageDiv.append(anchorImg);
+
+    let cardContent = createElement('div', 'cardContent');
+
+    let cardTitleAndPrice = createElement('div', 'cardtitleAndPrice');
+
+    let title = createElement('h1');
+    title.innerText = `${ad.brand.value} ${ad.model.value}`
+
+    let priceAndFavsIcon = createElement('div', 'priceAndFavsIcon');
+    let price = createElement('h1');
+    price.innerText = `${ad.price.value} ${ad.currency.value}`
+
+    let favIconDiv = createElement('div', 'favIconDiv');
+    let favIcon = createElement('img')
+    favIcon.src = 'assets/images/icons/listFav.svg';
+    favIcon.alt = 'fav icon image'
+
+    favIconDiv.append(favIcon);
+    priceAndFavsIcon.append(price, favIconDiv);
+    cardTitleAndPrice.append(title, priceAndFavsIcon)
+
+    let cardDescription = createElement('div', 'cardDescription');
+    let cardFeatures = createElement('p', 'cardFeatures');
+    cardFeatures.innerText = 'Характеристики: '
+
+
+
+    let cardExtras = createElement('p', 'cardFeatures');
+    cardExtras.innerText = 'Особености: '
+    let cardRegion = createElement('p');
+    cardRegion.innerText = 'Регион: '
+
+    for (const key in ad) {
+
+        if (key === 'brand' || key === 'model' || key === 'currency' || key === 'images') {
+            continue;
+        } else if (key === 'extras') {
+            for (const extra in ad.extras) {
+                if (ad.extras[extra].content.length > 0) {
+                    cardExtras.innerText += ad.extras[extra].content.join(', ');
+                    cardExtras.innerText += ', ';
+                }
+            }
+        } else if (key === 'region') {
+            if (ad.region.value) {
+                cardRegion.innerText += ad.region.value;
+            }
+        } else if (ad[key].value) {
+            cardFeatures.innerText += `${ad[key].category}: ${ad[key].value}, `
+        }
+    }
+
+    cardDescription.append(cardFeatures, cardExtras, cardRegion);
+
+    cardContent.append(cardTitleAndPrice, cardDescription);
+
+    upperBody.append(cardImageDiv, cardContent);
+
+    let cardDetails = createElement('div', 'cardDetails');
+    let cardMoreDetails = createElement('div', 'cardMoreDetails');
+    let anchMoreDetails = createElement('a');
+    anchMoreDetails.href = '#'
+    anchMoreDetails.innerText = `Повече детайли и ${ad.images.value.length} снимки`;
+    let spanMoreDetails = createElement('span');
+    spanMoreDetails.innerText = '|';
+    let anchAddInNotebook = createElement('a');
+    anchAddInNotebook.href = '#'
+    anchAddInNotebook.innerText = 'Добави в бележника';
+
+    cardMoreDetails.append(anchMoreDetails, spanMoreDetails, anchAddInNotebook);
+
+    let cardMark = createElement('div');
+    let labelMark = createElement('label');
+    labelMark.innerText = 'Маркирай обявата'
+    let checkboxMark = createElement('input');
+    checkboxMark.type = 'checkbox';
+
+    cardMark.append(labelMark, checkboxMark);
+
+    cardDetails.append(cardMoreDetails, cardMark);
+
+    card.append(upperBody, cardDetails);
+    mainResultsElement.append(card)
+}
+
+function createElement(type, className) {
+    let el = document.createElement(type);
+    if (className) {
+        el.classList.add(className);
+    }
+    return el;
+}
 
 function renderResultsMainInfo() {
     let brand = '';
@@ -169,16 +289,12 @@ function renderResultsMainInfo() {
     }
 
     function renderResultInfoHeading() {
-        let totalAds = resultObj.length;
-        console.log(resultObj);
+        let totalAds = resultsObj.length;
 
         resultInfoHeadingElement.innerText = `1 - 10 от общо ${totalAds} Обяви за ${brand} ${model} Автомобили и Джипове`
     }
 
 }
-
-
-
 
 function resetSearchMenu() {
     console.log('reseting...');
